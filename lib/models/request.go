59bfe2e9cd1930e97d9ef9f2cc98f24317bd6455
Admin Dans <bainandhika.baghaskara@dansmultipro.com>
2024-07-28 09:19:50 +0700

    add binding to add article request

--- lib/models/request.go
+++ lib/models/request.go
@@ -1,9 +1,9 @@
 package models
 
 type AddArticle struct {
-	Author string `json:"author"`
-	Title  string `json:"title"`
-	Body string `json:"body"`
+	Author string `json:"author" binding:"required"`
+	Title  string `json:"title" binding:"required"`
+	Body string `json:"body" binding:"required"`
 }
 
 type GetArticles struct {
