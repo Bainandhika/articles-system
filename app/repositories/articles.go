91dbf1b53fb1ab73077bf0a28ef0187f91e18cd3
Admin Dans <bainandhika.baghaskara@dansmultipro.com>
2024-07-28 08:35:38 +0700

    create repositories

---
+++ app/repositories/articles.go
@@ -0,0 +1,36 @@
+package repositories
+
+import (
+	"articles-system/lib/models"
+
+	"gorm.io/gorm"
+)
+
+type articlesRepo struct {
+	db *gorm.DB
+}
+
+type ArticlesRepo interface {}
+
+func NewArticlesRepo(db *gorm.DB) ArticlesRepo {
+    return &articlesRepo{db: db}
+}
+
+func (r *articlesRepo) Create(data models.Article) error {
+	return r.db.Create(&data).Error
+}
+
+func (r *articlesRepo) GetArticles(query, author string) ([]models.Article, error) {
+	var articles []models.Article
+
+    if query != "" {
+        query = "%" + query + "%"
+        r.db.Where("title LIKE ? OR body LIKE ?", query, query).Find(&articles)
+    } else if author != "" {
+        r.db.Where("author = ?", author).Find(&articles)
+    } else {
+        r.db.Find(&articles)
+    }
+
+    return articles, nil
+}
