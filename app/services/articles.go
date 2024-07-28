657155d71d030c6c3d0c676f8c3acbe2b9003749
Admin Dans <bainandhika.baghaskara@dansmultipro.com>
2024-07-28 09:02:39 +0700

    adjust return values

--- app/services/articles.go
+++ app/services/articles.go
@@ -3,32 +3,42 @@ package services
 import (
 	"articles-system/app/repositories"
 	"articles-system/lib/models"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 type articlesService struct {
 	repo repositories.ArticlesRepo
 }
 
-type ArticlesService interface {}
+type ArticlesService interface {
+	Create(payload models.AddArticle) *fiber.Error
+	GetArticles(queryParams models.GetArticles) ([]models.Article, *fiber.Error)
+}
 
 func NewArticlesService(repo repositories.ArticlesRepo) ArticlesService {
     return &articlesService{repo: repo}
 }
 
-func (s *articlesService) Create(payload models.AddArticle) error {
+func (s *articlesService) Create(payload models.AddArticle) *fiber.Error {
 	articleData := models.Article{
 		Author: payload.Author,
 		Title:  payload.Title,
         Body:   payload.Body,
 	}
 
-	return s.repo.Create(articleData)
+	err := s.repo.Create(articleData)
+	if err!= nil {
+        return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+    }
+
+	return nil
 }
 
-func (s *articlesService) GetArticles(queryParams models.GetArticles) ([]models.Article, error) {
+func (s *articlesService) GetArticles(queryParams models.GetArticles) ([]models.Article, *fiber.Error) {
 	articles, err := s.repo.GetArticles(queryParams.Query, queryParams.Author)
 	if err != nil {
-		return nil, err
+		return nil, fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
 
 	return articles, nil
