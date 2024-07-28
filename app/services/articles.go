6427fa3a56c220592547e8342da12ec9b5cfa136
Admin Dans <bainandhika.baghaskara@dansmultipro.com>
2024-07-28 09:09:17 +0700

    give detail to error

--- app/services/articles.go
+++ app/services/articles.go
@@ -3,6 +3,7 @@ package services
 import (
 	"articles-system/app/repositories"
 	"articles-system/lib/models"
+	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -17,28 +18,32 @@ type ArticlesService interface {
 }
 
 func NewArticlesService(repo repositories.ArticlesRepo) ArticlesService {
-    return &articlesService{repo: repo}
+	return &articlesService{repo: repo}
 }
 
 func (s *articlesService) Create(payload models.AddArticle) *fiber.Error {
+	domainFunc := "[ services.articles.Create ]"
+
 	articleData := models.Article{
 		Author: payload.Author,
 		Title:  payload.Title,
-        Body:   payload.Body,
+		Body:   payload.Body,
 	}
 
 	err := s.repo.Create(articleData)
-	if err!= nil {
-        return fiber.NewError(fiber.StatusInternalServerError, err.Error())
-    }
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, fmt.Sprintf("%s error creating article: %v", domainFunc, err))
+	}
 
 	return nil
 }
 
 func (s *articlesService) GetArticles(queryParams models.GetArticles) ([]models.Article, *fiber.Error) {
+	domainFunc := "[ services.articles.GetArticles ]"
+
 	articles, err := s.repo.GetArticles(queryParams.Query, queryParams.Author)
 	if err != nil {
-		return nil, fiber.NewError(fiber.StatusInternalServerError, err.Error())
+		return nil, fiber.NewError(fiber.StatusInternalServerError, fmt.Sprintf("%s error getting article: %v", domainFunc, err))
 	}
 
 	return articles, nil
